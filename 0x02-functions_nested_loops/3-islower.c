4316a8bb4a02f87ada2f6c1551c65417a2219999
JANBAX12 <henoksamson57@gmail.com>
2023-03-21 21:30:29 -0700

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,13 @@
  * _islower - Shows 1 if the input is a
  * lowecase character.  Another cases, shows
  * 0
- *
- * @c: The character in ASCII code
- *
- * Return 1for 
+ * @c:  input character
+ * Return: 1 for lowercase , Return 0 for otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
