0afcda4555f019a9952cd416b00fa504b5dd9e9f
JANBAX12 <henoksamson57@gmail.com>
2023-03-22 15:35:28 -0700

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,17 @@
 /**
  * _isalpha - checks for alphabetic character
  *
- * Return: Always 0.
+ * Return: 1 for if the caracter is alfabetic
+ * @c: input characeter
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
